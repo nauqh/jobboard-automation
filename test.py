9664e0e96f6d80f74054c4343c037c425031df08
Nauqh <hodominhquan.work@gmail.com>
2024-07-07 00:24:43 +0700

    test(scrapers): test topcv scraper

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import json
 import os
 from scrapers.itviec import scrape_jobs
+from scrapers.topcv import scrape_jobs_topcv
 
-BASE = "data/raw/"
+BASE = "data/raw2/"
 
 
 def save_jobs_to_json(job_data, file_path):
@@ -15,5 +16,13 @@ def save_jobs_to_json(job_data, file_path):
 itviec_jobs_data = scrape_jobs("https://itviec.com/it-jobs/data-analyst-sql")
 itviec_jobs_fsw = scrape_jobs("https://itviec.com/it-jobs/reactjs")
 
+topcv_jobs_data = scrape_jobs_topcv(
+    "https://www.topcv.vn/tim-viec-lam-data-analyst?exp=2")
+topcv_jobs_fsw = scrape_jobs_topcv(
+    "https://www.topcv.vn/tim-viec-lam-reactjs?exp=2")
+
+
 save_jobs_to_json(itviec_jobs_data, BASE + "itviec_jobs_data.json")
 save_jobs_to_json(itviec_jobs_fsw, BASE + "itviec_jobs_fsw.json")
+save_jobs_to_json(topcv_jobs_data, BASE + "topcv_jobs_data.json")
+save_jobs_to_json(topcv_jobs_fsw, BASE + "topcv_jobs_fsw.json")
