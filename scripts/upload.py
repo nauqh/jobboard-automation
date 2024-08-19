d6cc77ee0f77067a13d4033cc49a106dae56c30c
Nauqh <hodominhquan.work@gmail.com>
2024-08-20 04:34:35 +1000

    update(upload): add api password

--- scripts/upload.py
+++ scripts/upload.py
@@ -2,6 +2,10 @@ import os
 import json
 import requests
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def get_subfolder_names(path=None):
     if path is None:
@@ -13,14 +17,16 @@ def get_subfolder_names(path=None):
 if __name__ == "__main__":
     # NOTE: Upload jobs
     URL = "https://jobboard.up.railway.app"
-    BASE = 'data/filter'
-    subfolders = get_subfolder_names(BASE)
 
-    for path in get_subfolder_names('data/filter/'):
+    for path in get_subfolder_names('data/processed/'):
         tag = 'data' if 'data' in path else 'fsw'
-        with open(os.path.join('data/filter/', path), 'r', encoding='utf-8') as file:
+        with open(os.path.join('data/processed/', path), 'r', encoding='utf-8') as file:
             data = json.load(file)
             for item in data:
                 item['tag'] = tag
-            response = requests.post(URL + "/jobs", json=data)
+            headers = {
+                'password': os.environ['API_PWD'],
+                'Content-Type': 'application/json'
+            }
+            response = requests.post(URL + "/jobs", json=data, headers=headers)
             print(response.text)
